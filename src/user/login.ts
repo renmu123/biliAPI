e057904d54434dff8b54d63ab2fbd9767cd9dacf
muse <fyx@030buy.jp>
2023-12-14 20:39:59 +0800

    WIP

--- src/user/login.ts
+++ src/user/login.ts
@@ -5,50 +5,6 @@ import { md5 } from "~/utils/index.ts";
 
 import type { Request, CommonResponse } from "~/types/index.d.ts";
 
-/**
- * 获取登录二维码
- * @param _request 实例
- * @returns
- */
-export function generate_qrcode(_request: Request): Promise<
-  CommonResponse<{
-    url: string;
-    qrcode_key: string;
-  }>
-> {
-  return _request.get(
-    `https://passport.bilibili.com/x/passport-login/web/qrcode/generate?source=main-fe-header`
-  );
-}
-
-/**
- * 轮询二维码状态
- * @param _request 实例
- * @param qrcode_key generate_qrcode获取的key
- * @returns
- */
-export function poll_qrcode(
-  _request: Request,
-  qrcode_key: string
-): Promise<
-  CommonResponse<{
-    code: 0 | 86101; // 0: 成功; 86101: 待扫码;86038: 二维码已过期;86090: 二维码已扫码未确认
-    message: string;
-    refresh_token: string;
-    timestamp: number;
-    url: string;
-  }>
-> {
-  return _request.get(
-    `https://passport.bilibili.com/x/passport-login/web/qrcode/poll`,
-    {
-      params: {
-        qrcode_key: qrcode_key,
-      },
-    }
-  );
-}
-
 const enum Event {
   start = "start",
   scan = "scan",
@@ -56,6 +12,9 @@ const enum Event {
   error = "error",
   end = "end",
 }
+/**
+ * TV扫码登录
+ */
 export class TvQrcodeLogin extends BaseRequest {
   private appkey = "4409e2ce8ffd12b8";
   private secretKey = "59b43e04ad6965f34319062b478f83dd";
@@ -134,7 +93,7 @@ export class TvQrcodeLogin extends BaseRequest {
     const timer = setInterval(async () => {
       const response = await this.poll(auth_code);
       // 0：成功
-      // 86039 二维码尚未确认
+      // 86039 二维码尚未扫描
       // 86038：二维码已失效
       // 86090：二维码已扫码未确认
 
@@ -224,5 +183,141 @@ export class TvQrcodeLogin extends BaseRequest {
       .join("&");
     const signString = `${queryString}${this.secretKey}`;
     return md5(signString);
+  }
+}
+
+/**
+ * 网页扫码登录
+ */
+export class WebQrcodeLogin extends BaseRequest {
+  private timmer: NodeJS.Timeout | null = null;
+  emitter = new EventEmitter();
+  constructor() {
+    super({
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        cookie: null,
+      },
+    });
+  }
+
+  /**
+   * 获取登录二维码
+   */
+  async getQrcode(): Promise<
+    CommonResponse<{
+      url: string;
+      qrcode_key: string;
+    }>
+  > {
+    return this.request.post(
+      "https://passport.bilibili.com/x/passport-login/web/qrcode/generate"
+    );
+  }
+  /**
+   * 轮询二维码状态
+   * @param auth_code
+   */
+  poll(qrcode_key: string) {
+    return this.request.get<never, CommonResponse<any>>(
+      "https://passport.bilibili.com/x/passport-login/web/qrcode/poll",
+      {
+        params: {
+          qrcode_key: qrcode_key,
+        },
+      }
+    );
+  }
+
+  async login() {
+    const res = await this.getQrcode();
+    if (res.code !== 0) {
+      throw new Error(res.message);
+    }
+    const qrcode_key = res.data.qrcode_key;
+
+    this.emitter.emit("start");
+    let count = 0;
+    const timer = setInterval(async () => {
+      const response = await this.poll(qrcode_key);
+      // 0：成功
+      // 86101 二维码尚未扫描
+      // 86038：二维码已失效
+      // 86090：二维码已扫码未确认
+
+      if (response.code === 0) {
+        this.emitter.emit(Event.completed, response);
+        this.emitter.emit(Event.end, response);
+        clearInterval(timer);
+        this.removeAllListeners();
+      } else if (response.code === 86038) {
+        this.emitter.emit(Event.end, response);
+        this.emitter.emit(Event.error, response);
+        clearInterval(timer);
+        this.removeAllListeners();
+      } else if (response.code === 86101 || response.code === 86090) {
+        this.emitter.emit(Event.scan, response);
+      } else {
+        this.emitter.emit(Event.end, response);
+        this.emitter.emit(Event.error, response);
+        clearInterval(timer);
+        this.removeAllListeners();
+      }
+      count++;
+      if (count > 180) {
+        this.emitter.emit(Event.end, {
+          code: 86038,
+          message: "二维码已失效",
+          ttl: 1,
+          data: null,
+        });
+        this.emitter.emit(Event.error, {
+          code: 86038,
+          message: "二维码已失效",
+          ttl: 1,
+          data: null,
+        });
+        clearInterval(timer);
+        this.emitter.emit(Event.end, response);
+      }
+    }, 1000);
+    this.timmer = timer;
+
+    return res.data.url;
+  }
+  /**
+   * 中断任务，并清除所有监听器
+   */
+  interrupt() {
+    clearInterval(this.timmer);
+    this.removeAllListeners();
+  }
+  /**
+   * 监听事件
+   */
+  on(event: keyof typeof Event, callback: (response: any) => void) {
+    this.emitter.on(event, callback);
+  }
+  /**
+   * 监听事件，只触发一次
+   */
+  once(event: keyof typeof Event, callback: (response: any) => void) {
+    this.emitter.once(event, callback);
+  }
+  /**
+   * 移除监听事件
+   */
+  off(event: keyof typeof Event, callback: (response: any) => void) {
+    this.emitter.off(event, callback);
+  }
+  /**
+   * 移除所有监听事件
+   */
+  removeAllListeners(event?: keyof typeof Event) {
+    if (event) {
+      this.emitter.removeAllListeners(event);
+    } else {
+      this.emitter.removeAllListeners();
+    }
   }
 }
